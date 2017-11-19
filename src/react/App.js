45627ff6c1e749b5ce0827aa5c4d4c8ffb95eb14
Luigi Lake <lfdlake@gmail.com>
2017-11-19 14:26:19 -0500

    create initial about me page

--- src/react/App.js
+++ src/react/App.js
@@ -3,12 +3,14 @@ import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 import '../assets/styles/App.scss';
 
 import NavBar from './containers/NavBar'
+import AboutMe from './containers/AboutMe'
 
 
 const App = props => {
   return (
     <Router history={browserHistory}>
       <Route path='/' component={NavBar}>
+        <IndexRoute component={AboutMe}/>
       </Route>
     </Router>
   );
